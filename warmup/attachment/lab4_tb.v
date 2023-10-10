0e04a78fc1cb71b237ca769dbf0a660e9eebb5e6
Gua <guahao@zju.edu.cn>
2023-10-10 15:15:20 +0800

    Deployed 75ab042 with MkDocs version: 1.5.2

---
+++ warmup/attachment/lab4_tb.v
@@ -0,0 +1,32 @@
+`timescale 1ns/1ns
+
+module lab4_tb ();
+
+    reg I0;
+    reg I1;
+    reg I2;
+
+    wire res;
+
+    main test_inst (
+        .I0(I0),
+        .I1(I1),
+        .I2(I2),
+        .res(res)
+    );
+
+    initial begin
+        #5;
+        I0 = 1'b0; I1 = 1'b0; I2 = 1'b0; #5;
+        I0 = 1'b0; I1 = 1'b0; I2 = 1'b1; #5;
+        I0 = 1'b0; I1 = 1'b1; I2 = 1'b0; #5;
+        I0 = 1'b0; I1 = 1'b1; I2 = 1'b1; #5;
+        I0 = 1'b1; I1 = 1'b0; I2 = 1'b0; #5;
+        I0 = 1'b1; I1 = 1'b0; I2 = 1'b1; #5;
+        I0 = 1'b1; I1 = 1'b1; I2 = 1'b0; #5;
+        I0 = 1'b1; I1 = 1'b1; I2 = 1'b1; #5;
+        #5;
+        I0 = 1'b0; I1 = 1'b0; I2 = 1'b0; #5;
+    end
+
+endmodule
